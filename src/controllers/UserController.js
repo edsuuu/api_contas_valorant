95f9e849aa0ca2c3e205dc0e5460a6deda32b286
edsuu <111332935+edsuuu@users.noreply.github.com>
2024-04-10 18:32:10 -0300

    Refatorando o ModelUser/Controller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,8 +1,38 @@
-import User from "../models/UserModel";
+import UserModel from "../models/UserModel";
 
 class UserController {
      async store(req, res) {
-        
+          try {
+               const novoUsuario = new UserModel(req.body);
+
+               await novoUsuario.validate();
+
+               await novoUsuario.save();
+
+               res.status(201).json({
+                    msg: 'Usuário criado com sucesso !'
+               });
+
+          } catch (error) {
+               if (error.name === 'ValidationError') {
+                    const validationErrors = Object.values(error.errors).map(err => err.message);
+                    res.status(400).json({ errors: validationErrors });
+               } else if (error.code === 11000) {
+                    res.status(400).json({ error: 'Email já existe' });
+               } else {
+                    console.error('Erro ao criar Usuário:', error);
+                    res.status(500).json({ error: 'Erro interno do servidor' });
+               }
+          }
+     }
+     async index(req, res) {
+          try {
+               const listarUsuarios = await UserModel.find({})//,{ password: 0, __v: 0 });
+
+               return res.json(listarUsuarios);
+          } catch (e) {
+               return res.json(null);
+          }
      }
 
 }
--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,23 +1,58 @@
 import mongoose from 'mongoose';
-const validator = require('validator');
-const bcryptjs = require('bcryptjs');
+import bcryptjs from 'bcryptjs';
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
      nome: {
           type: String,
-          required: true,
+          default: '',
+          required: [true, 'Campo nome é obrigatorio'],
+          validate: {
+               validator: function (v) {
+                    return v.length >= 3 && v.length <= 255;
+               },
+               message: 'Campo nome precisa ter entre 3 a 255 caracteres'
+          },
+          trim: true,
      },
      email: {
           type: String,
-          required: true
+          default: '',
+          required: [true, 'Campo e-mail é obrigatorio'],
+          unique: true,
+          validate: {
+               validator: (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email),
+               message: 'Email inválido',
+          },
      },
-     password: {
+     password_hash: {
           type: String,
-          required: true
+          default: '',
+     },
+});
+
+UserSchema.virtual('password')
+  .set(function (password) {
+    this._password = password;
+    if (password.length < 6 || password.length >= 30) {
+      throw new Error('Campo senha tem que ter entre 6 a 30 caracteres');
+    }
+    this.password_hash = bcryptjs.hashSync(password, 10);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+  UserSchema.pre('save', async function (next) {
+     if (this.isModified('password')) {
+       this.password_hash = await bcryptjs.hash(this.password, 10);
      }
-})
+     next();
+   });
 
-const UserModel = mongoose.model('user', userSchema);
+UserSchema.methods.passwordIsValid = async function (password) {
+  return bcryptjs.compare(password, this.password_hash);
+};
 
+const UserModel = mongoose.model('users', UserSchema);
 
 export default UserModel;
