7282abc9a19ad83b0e045612d274bd4292393df9
João Victor <88406996+joaosouzaa01@users.noreply.github.com>
2024-04-14 22:30:34 -0300

    Update ContasController.js

--- src/controllers/ContasController.js
+++ src/controllers/ContasController.js
@@ -33,7 +33,6 @@ class ContaController {
      async index(req, res) {
           try {
                const listarContas = await ContaModel.find({}, { senha_conta: 0, __v: 0 });
-
                return res.status(200).json(listarContas);
           } catch (e) {
                return res.json(null);
