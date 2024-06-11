12fb1ccca5ebafe3ca7264595cb9002e9724d822
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-10 21:03:26 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import App from './app'
 
-// const port = process.env.APP_PORT;
+const port = process.env.APP_PORT;
 
-App.listen();
+App.listen(port, () => {
+     console.log();
+     console.log(`Server rodando em http://localhost:${port}`);
+});
